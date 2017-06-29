403fc68a4f2842f313310497994ac2e3bb090d5b
Илья <ilya_celuyko@mail.ru>
2017-06-29 19:54:07 +0300

    ОКОК

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("TI PIROD");
+            Console.WriteLine("нет я не пидор");
         }
     }
 }
